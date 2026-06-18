1f4f08f78dca109eb06b537a9b4df339ebfb9f5e
yahyanursidik <yahyanursidik@gmail.com>
2026-06-19 00:06:25 +0700

    fix: remove mobile bottom nav from admin layout in favor of sidebar drawer

--- src/components/layout/AdminLayout.tsx
+++ src/components/layout/AdminLayout.tsx
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import { Outlet } from "react-router-dom";
 import { Sidebar } from "./Sidebar";
 import { Topbar } from "./Topbar";
-import { MobileBottomNav } from "./MobileBottomNav";
 import { Toaster } from "sonner";
 
 export const AdminLayout: React.FC = () => {
@@ -13,13 +12,12 @@ export const AdminLayout: React.FC = () => {
       <Sidebar isOpen={isMobileMenuOpen} onClose={() => setIsMobileMenuOpen(false)} />
       <div className="flex-1 flex flex-col min-w-0">
         <Topbar onMenuClick={() => setIsMobileMenuOpen(true)} />
-        <main className="flex-1 p-4 md:p-8 overflow-y-auto pb-24 md:pb-8">
+        <main className="flex-1 p-4 md:p-8 overflow-y-auto">
           <div className="max-w-7xl mx-auto w-full">
             <Outlet />
           </div>
         </main>
       </div>
-      <MobileBottomNav />
       <Toaster position="top-right" richColors />
     </div>
   );
